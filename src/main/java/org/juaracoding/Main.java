0cc481d8cb85bb36156f6675356529f435f92cdb
joeandreas <joeindahsi@gmail.com>
2023-10-31 09:52:14 +0700

    penambahan package jaijavafundamental

---
+++ src/main/java/org/juaracoding/Main.java
@@ -0,0 +1,10 @@
+package org.juaracoding;
+
+public class Main {
+    public static void main(String[] args) {
+        String strSambung = "8"+3;
+
+        System.out.println(strSambung);
+
+    }
+}
---
+++ src/main/java/org/juaracoding/jaijavafundamental/testharian/logikapemrograman/LogikaPemrograman.java
@@ -0,0 +1,15 @@
+package org.juaracoding.jaijavafundamental.testharian.logikapemrograman;
+import java.util.Scanner;
+
+public class LogikaPemrograman {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String kata = "";
+        System.out.print("Masukan kata : ");
+        kata = scan.nextLine();
+
+        // ????
+
+        System.out.println("Hasil : " );
+    }
+}
